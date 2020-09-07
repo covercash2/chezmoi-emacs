663b1c52629f1ede37dcd912294ed0dd2859353f
Chris Overcash <covercash2@gmail.com>
2020-09-07 01:27:36 -0500

    chezmoi edit now uses chezmoi-source-path

--- chezmoi.el
+++ chezmoi.el
@@ -23,22 +23,14 @@
     )
   )
 
-(defun print-chezmoi-source-path ()
-  (message (chezmoi-source-path "~/.config")))
-
 (defun chezmoi-edit (file)
   "Edit a file managed by chezmoi.
 This function should work just like `chezmoi edit'
 FILE points to the destination file."
   (interactive "fConfig file: ")
   (let ((chezmoi-file
-	 (string-trim ; requires emacs 24.4
-	  (shell-command-to-string
-	   (concat "chezmoi source-path " file)))))
-    (cond ((file-exists-p chezmoi-file)
-	   (find-file chezmoi-file))
-	  (t (error "Unable to find source config file: %s" chezmoi-file)))
-    ))
+	 (chezmoi-source-path file)))
+    (find-file chezmoi-file)))
 
 (defun chezmoi-apply ()
   "Apply chezmoi config."
