f87f6e2e2549615390883e810e43329c51734dd9
covercash2 <covercash2@gmail.com>
2020-09-20 00:53:05 -0500

    add license to chezmoi.el

--- chezmoi.el
+++ chezmoi.el
@@ -1,4 +1,23 @@
 ;;; chezmoi.el --- chezmoi management
+
+;;; Copyright (C) 2020 Chris Overcash
+
+;;; Author: Chris Overcash <covercash2@gmail.com>
+;;; Version: 0.1.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;; In this file "source" is used to describe files
 ;;; that exist in the chezmoi repo.
